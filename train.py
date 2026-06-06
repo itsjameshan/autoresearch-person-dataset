26decf2756b182d2eff5d8178fe69ce24fa4c175
itsjameshan <itsjameshan@users.noreply.github.com>
2026-06-06 15:10:29 +0800

    exp_002: [HPO] CDS=0.0000 P=0.0000 R=0.0000
    run_id=run_9f02fc3f_20260606_151023
    iteration=2
    best_params={}

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ DATA_YAML = "D:\\PythonProject\\person_dataset\\person.yaml"
 IMGSZ = 640
 EPOCHS = 2
 
-BATCH = 30
+BATCH = 16
 import torch
 
 # Auto-detect compatible device (handles RTX 50-series sm_120 compatibility)
@@ -44,7 +44,7 @@ SINGLE_CLS = True
 COS_LR = True
 PATIENCE = 30
 
-LR0 = 0.006177430943619523
+LR0 = 0.0010771206028005972
 LRF = 0.0002
 HSV_H = 0.015
 HSV_S = 0.7
@@ -54,13 +54,13 @@ TRANSLATE = 0.3
 SCALE = 0.5
 FLIPUD = 0.0
 FLIPLR = 0.5
-MOSAIC = 0.3681521095156265
+MOSAIC = 0.46856304467030596
 MIXUP = 0.1
 COPY_PASTE = 0.1
 ERASING = 0.4
 CLOSE_MOSAIC = 15
 
-BOX = 10.197112905315404
+BOX = 8.265998128879701
 CLS = 1.0
 
 CONF = 0.001
